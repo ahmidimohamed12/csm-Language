a8d5be7b2faf078a624d70bc3b032e1c33092d03
Ahmidi Mohamed <ahmidinador123@gmail.com>
2019-12-25 05:41:52 +0100

    add

---
+++ csml_ap/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string cmd;
+        static void affconsol()
+        {
+            //Console.WriteLine("=================== CSML =========================\n");
+            //Console.WriteLine("\n\n\n");
+            //for (int i = 0; i < 100000000; i++)
+            //{
+            //    Console.Write("CSML==>\t");
+            //    cmd = Console.ReadLine();
+            //    if (cmd == "Exit")
+            //        break;
+            //    if (cmd == "Clear")
+            //    {
+            //        Console.Clear();
+            //        Console.WriteLine("=================== CSML =========================\n");
+            //    }
+
+            ////}
+            //if ("")
+            //Console.WriteLine();
+        }
+       public  static void afl(string s)
+        {
+            if (s == "console()")
+                Console.WriteLine("Hello");
+
+        }
+        static void Main(string[] args)
+        {
+           // affconsol();
+            Form fr = new Form();
+            fr.Width = 1000;
+            fr.Height = 1500;
+            fr.BackColor = Color.White;
+            
+            // Button Compiler
+            Button btn = new Button();
+            btn.Location = new Point(200, 20);
+            btn.Text = "hy";
+          
+            fr.Controls.Add(btn);
+            //
+           
+            //
+            RichTextBox ls = new RichTextBox();
+            ls.Location = new Point(100, 50);
+            ls.Width = 500;
+            ls.Height = 1700;
+
+            btn.Click += (s, e) => {
+                    Console.WriteLine("Hy");
+                
+                //afl(ls.Text);
+            };
+            fr.Controls.Add(ls);
+            Application.Run(fr);
+
+            
+        }
+    }
+}
